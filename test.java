37b904f5749fa438e334192847ff3d877a42e77d
bs149808 <bs149808@ohio.edu>
2012-10-04 03:32:20 -0400

    testing

--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+
 public class test {
 
   public static void main(String[] args) {
@@ -5,7 +6,29 @@ public class test {
     int b = 16;
     int c = 20;
 
-    AddSub obj = new AddSub();
+    AddSub obj = new aAddSub();
 
-    System.out.format("%d + %d = %d\n", a, b, obj.add(a,b));
-    System.out.format("%
+    System.out.format("%d + %d = %d\n", a, b, obj.addd(a,b));
+    System.out.format("%d + %d = %d\n", -a, b, obj.addd(-a,b));
+    System.out.format("%d - %d = %d\n", a, b, obj.subtract(a,b));
+    System.out.format("%d - %d = %d\n", -a, b, obj.subtract(-a,b));
+
+    //Test added by Zach for Part 3
+    System.out.format("%d - %d = %d\n", -c, b, obj.subtract(-c,b));
+    
+    // Test Cases added by Bryan Schneider (the partner) for Part 3
+   int x = 8;
+   int y = 1;
+   System.out.format("%d + %d = %d\n", x, y, obj.add(x,y));
+   System.out.format("%d + %d = %d\n", -x, y, obj.add(-x,y));
+   System.out.format("%d - %d = %d\n", x, -y, obj.add(x,-y));
+   System.out.format("%d - %d = %d\n", -x, -y, obj.add(-x,-y));
+   System.out.format("%d + %d = %d\n", x, y, obj.subtract(x,y));
+   System.out.format("%d + %d = %d\n", -x, y, obj.subtract(-x,y));
+   System.out.format("%d - %d = %d\n", x, -y, obj.subtract(x,-y));
+   System.out.format("%d - %d = %d\n", -x, -y, obj.subtract(-x,-y));
+   // End of partner's tampering
+  }
+
+}
+
