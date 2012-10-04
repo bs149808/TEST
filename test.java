3dc80858356a6f0ab7ee7d1b5384d176bb31411d
bs149808 <bs149808@ohio.edu>
2012-10-04 03:25:20 -0400

    testing

--- test.java
+++ test.java
@@ -1,1 +1,11 @@
-sdadasdadadadadadad
+public class test {
+
+  public static void main(String[] args) {
+    int a = 7;
+    int b = 16;
+    int c = 20;
+
+    AddSub obj = new AddSub();
+
+    System.out.format("%d + %d = %d\n", a, b, obj.add(a,b));
+    System.out.format("%
